b94494923e4c54a59fc816a4cd20cf98fa00037f
Gregory N. Schmit <schmitgreg@gmail.com>
2023-01-11 01:55:38 -0600

    Fix bad usage of legacy_connection_handling in Rails <7.

--- test/config/application.rb
+++ test/config/application.rb
@@ -34,12 +34,11 @@ class Application < Rails::Application
   config.cache_classes = false
   config.action_controller.perform_caching = false
 
-  config.active_record.legacy_connection_handling = false
-
   RESTFramework.config.freeze_config = true
 
   if Rails::VERSION::MAJOR >= 7
     config.active_support.remove_deprecated_time_with_zone_name = true
+    config.active_record.legacy_connection_handling = false
   end
 
   if USE_SPROCKETS
