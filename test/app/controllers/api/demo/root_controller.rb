8ff0ae8acf3e078a54e8749e6a1de3d85d7050f8
Gregory N. Schmit <schmitgreg@gmail.com>
2024-04-06 18:50:02 -0500

    Attempt to properly pass PRODUCTION_KEY to Kamal deployment.

--- test/app/controllers/api/demo/root_controller.rb
+++ test/app/controllers/api/demo/root_controller.rb
@@ -1,23 +1,30 @@
 class Api::Demo::RootController < Api::DemoController
-  self.extra_actions = {nil: :get, blank: :get, echo: :post, secret_key_base: :get}
+  self.extra_actions = {nil: :get, blank: :get, echo: :post, debugging: :get}
 
   def root
-    api_response({message: Api::DemoController::DESCRIPTION})
+    return api_response({message: Api::DemoController::DESCRIPTION})
   end
 
   def nil
-    api_response(nil)
+    return api_response(nil)
   end
 
   def blank
-    api_response("")
+    return api_response("")
   end
 
   def echo
-    api_response({message: "Here is your data:", data: request.request_parameters})
+    return api_response({message: "Here is your data:", data: request.request_parameters})
   end
 
-  def secret_key_base
-    api_response({secret_key_base: Rails.application.secret_key_base})
+  def debugging
+    return api_response(
+      {
+        secret_key_base: Rails.application.secret_key_base,
+        secret_key_base_from_credentials: Rails.application.credentials.secret_key_base,
+        secret_key_base_from_env: ENV["SECRET_KEY_BASE"],
+        production_key: ENV["PRODUCTION_KEY"],
+      },
+    )
   end
 end
