8f36d1ec851629ecd74f2c9ca8de59036bbdb85a
Gregory N. Schmit <schmitgreg@gmail.com>
2024-04-06 20:28:15 -0500

    Add Sentry release info.

--- test/config/initializers/sentry.rb
+++ test/config/initializers/sentry.rb
@@ -1,6 +1,7 @@
 if defined?(Sentry) && dsn = Rails.application.credentials.sentry_dsn
   Sentry.init do |config|
     config.dsn = dsn
+    config.release = "rest_framework@#{RESTFramework::VERSION}"
     config.breadcrumbs_logger = [:active_support_logger, :http_logger]
 
     config.enable_tracing = true
