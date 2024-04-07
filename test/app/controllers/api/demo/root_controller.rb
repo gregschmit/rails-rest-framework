405ff6e60a3f922f0d3811831385ba8e815637f9
Gregory N. Schmit <schmitgreg@gmail.com>
2024-04-06 19:51:28 -0500

    Cleanup and rotate production keys.

--- test/app/controllers/api/demo/root_controller.rb
+++ test/app/controllers/api/demo/root_controller.rb
@@ -1,30 +1,19 @@
 class Api::Demo::RootController < Api::DemoController
-  self.extra_actions = {nil: :get, blank: :get, echo: :post, debugging: :get}
+  self.extra_actions = {nil: :get, blank: :get, echo: :post}
 
   def root
-    return api_response({message: Api::DemoController::DESCRIPTION})
+    api_response({message: Api::DemoController::DESCRIPTION})
   end
 
   def nil
-    return api_response(nil)
+    api_response(nil)
   end
 
   def blank
-    return api_response("")
+    api_response("")
   end
 
   def echo
-    return api_response({message: "Here is your data:", data: request.request_parameters})
-  end
-
-  def debugging
-    return api_response(
-      {
-        secret_key_base: Rails.application.secret_key_base,
-        secret_key_base_from_credentials: Rails.application.credentials.secret_key_base,
-        secret_key_base_from_env: ENV["SECRET_KEY_BASE"],
-        production_key: ENV["PRODUCTION_KEY"],
-      },
-    )
+    api_response({message: "Here is your data:", data: request.request_parameters})
   end
 end
