e70e9417d3ab1d6efd8700dd8b97a5cf2a06dfdd
Gregory N. Schmit <schmitgreg@gmail.com>
2026-03-02 17:09:17 -0600

    Fix Sentry config.

--- test/config/initializers/sentry.rb
+++ test/config/initializers/sentry.rb
@@ -4,7 +4,6 @@ if defined?(Sentry) && dsn = Rails.application.credentials.sentry_dsn
     config.release = "rest_framework@#{RESTFramework::VERSION}"
     config.breadcrumbs_logger = [ :active_support_logger, :http_logger ]
 
-    config.enable_tracing = true
     config.traces_sample_rate = 1.0
     config.profiles_sample_rate = 1.0
   end
