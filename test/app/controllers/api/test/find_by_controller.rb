b0561d260dd0cf6b19c4b4b3cd8ce85a12df1ec5
Gregory N. Schmit <schmitgreg@gmail.com>
2026-04-22 09:53:58 -0500

    Fix rubocop

--- test/app/controllers/api/test/find_by_controller.rb
+++ test/app/controllers/api/test/find_by_controller.rb
@@ -3,5 +3,5 @@ class Api::Test::FindByController < Api::TestController
   self.fields = %w[id login age balance]
   self.find_by_fields = %w[login]
   self.filter_recordset_before_find = false
-  self.excluded_actions = [:create, :update, :destroy, :update_all, :destroy_all]
+  self.excluded_actions = [ :create, :update, :destroy, :update_all, :destroy_all ]
 end
